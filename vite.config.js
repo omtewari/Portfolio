0f48f169f1ea5639a0d3d7f20ba82ed98ba38268
Om Tewari <omtewari106@gmail.com>
2024-07-13 00:19:06 +0530

    Your commit message here

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-icons/fa','']
-    }
-  }
+      external: [
+        'react',                // React itself
+        'react-dom',            // React DOM
+        'react-icons/fa',       // FontAwesome icons
+        'framer-motion',        // Framer Motion
+        'axios',                // Axios for HTTP requests
+        'lodash',               // Lodash utility library
+        'moment',
+        'react-icons/fa6'               // Moment.js for date/time handling
+        // Add other external modules as needed
+      ],
+    },
+  },
 })
-
