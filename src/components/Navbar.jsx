50bd8217a8cbecaec49d953059a428f3ffdc5966
Om Tewari <omtewari106@gmail.com>
2024-07-12 23:40:25 +0530

    hello

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import logo from "../assets/logo.png";
-import {FaLinkedin} from "react-icons/fa"
-import {FaSquareXTwitter} from "react-icons/fa6"
-import {FaGithub} from "react-icons/fa"
+// import {FaLinkedin} from "react-icons/fa"
+// import {FaSquareXTwitter} from "react-icons/fa6"
+// import {FaGithub} from "react-icons/fa"
 const Navbar = () => {
   return (
     <nav className=" mb-20 flex items-center justify-between py-6 mt-0">
@@ -9,9 +9,9 @@ const Navbar = () => {
             <img src={logo} alt="logo" />
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-            <a href="https://www.linkedin.com/in/om-tewari-1b0b78201/ " target="blank"><FaLinkedin/></a>
+            {/* <a href="https://www.linkedin.com/in/om-tewari-1b0b78201/ " target="blank"><FaLinkedin/></a>
             <a href="https://x.com/OTewari1006" target="blank"><FaSquareXTwitter/></a>
-            <a href="https://github.com/omtewari" target="blank"><FaGithub/></a>
+            <a href="https://github.com/omtewari" target="blank"><FaGithub/></a> */}
         </div>
     </nav>
   )
